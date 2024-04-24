79a6e12cb73eed92a97149c792820f5393b27633
Betsy Kamas <kamase@sunypoly.edu>
2024-04-24 13:57:59 -0400

    mouse.h

---
+++ Mouse.h
@@ -0,0 +1,34 @@
+#ifndef MOUSE_H
+#define MOUSE_H
+#include <SDL.h>
+
+class Mouse{
+public:
+  SDL_Renderer rend;
+  SDL_Texture* tmouse;
+  SDL_Rect rect;
+  SDL_Rect point;
+
+  Mouse(SDL_Renderer* rend){
+    this->rend = rend;
+    tmouse = IMG_LoadTexture(rend, ".png");
+    rect.w = 25;
+    rect.h = 25;
+    point.w = 1;
+    point.h = 1;
+    SDL_ShowCursor(false);
+  }
+
+  void update(){
+    SDL_GetMouseState(&rect.x, &rect.y);
+    point.x = rect.x;
+    point.y = rect.y;
+  }
+
+  void draw(){
+    SDL_RenderCopy(rend, tmouse, NULL, &rect);
+  }
+
+};
+
+#endif
